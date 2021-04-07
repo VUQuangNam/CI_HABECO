9dd962920c01a347fa26458893d0b55f02624414
VQN <nam.vq@consultindochina.com>
2021-04-07 09:38:52 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'habeco';
+export class AppComponent implements OnInit {
+    title = 'habeco';
+    constructor(
+        private http: HttpClient
+    ) { }
+
+    listData: any = [];
+
+    ngOnInit() {
+        this.http.get('https://mxupvjfqlh.execute-api.ap-southeast-1.amazonaws.com/prod/scan?pageNumber=20&pageSize=0')
+            .subscribe((res: any) => {
+                this.listData = res.payload.map(x => {
+                    return {
+                        ScanId: x.ScanId,
+                        CreatedOn: x.CreatedOn,
+                        ItemId: x.ItemId,
+                        href: '#collapse1-' + x.ScanId,
+                        id: 'collapse1-' + x.ScanId,
+                        dataSub: []
+                    }
+                });
+            })
+    }
+
+
+    getDetailScan = (i) => {
+        this.http.get(`https://mxupvjfqlh.execute-api.ap-southeast-1.amazonaws.com/prod/item?itemId=${i.ItemId}`)
+            .subscribe((res: any) => {
+                i.dataSub = res.payload;
+            })
+
+    }
 }
